3d924f8b2eee3d1ae886ee71a9d619fd80001e81
SebastianCharmot <46443969+SebastianCharmot@users.noreply.github.com>
2019-08-29 15:08:11 -0400

    Add files via upload
    All functionalities working

--- node4.py
+++ node4.py
@@ -7,12 +7,12 @@ class Node:
         self.infected = infected
         self.id = id
         uninfected_nodes.append(self)
+        polynomial.append(0)
     def addNeighbor(self, neighborNode):
         self.neighbors.append(neighborNode)
 
 uninfected_nodes = []
 polynomial = []
-xpower = 0
 
 def generatecombinations():
     from itertools import combinations
@@ -25,29 +25,21 @@ def generatecombinations():
         for combination in combinations(possibilities, r):
             combs.append(list(combination))
         r += 1
-    print(combs)
+    # print(combs)
     for i in range(len(combs)):
-        # print(combs[i])
         infect(combs[i]) #pass into infect the values from combinations that it wants to infect
         for node in uninfected_nodes:
-            node.infected = False #set state back to everything uninfected
+            node.infected = False #set nodes back to uninfected
 
 def infect(victims):
     operating_nodes = uninfected_nodes
     xpower = len(victims)
     for victim in victims:
         operating_nodes[victim].infected = True
-    # for node in operating_nodes:
-    #     print(node.id, node.infected)
-    print(xpower)
-    print(victims)
-    forcing(operating_nodes)
+    forcing(operating_nodes, xpower) #pass on list of Node objects with inital nodes infected
 
-    #pass on list of Node objects with certain nodes infected to function forcing
-    return True
-
-def forcing(graph):
-    forced = 0
+def forcing(graph, xpower):
+    forced = 0 #Keep track of whether or not something was forced when looping through graph
     for node in graph:
         count = 0
         if node.infected == True:
@@ -60,41 +52,51 @@ def forcing(graph):
                 for neighbor in node.neighbors:
                     neighbor.infected = True
                     forced += 1
-                    # forcing(graph)
-    if forced == 1:
-        forcing(graph)
-    else: 
-        for node in graph:
-            print(node.id, node.infected)
-        print("-----------")
-
-    #run logic of forcing  
+    if forced >= 1: #If something was forced then we want to do forcing logic again
+        forcing(graph, xpower) #Can potentially be used to see how many forces it takes 
+    else: #After going through the entire graph no nodes were forced, so we are done forcing
+        check(graph,xpower)
     #force until no change then goes to function forced
-    return True
 
-def forced():
-    #check whether each node is forced or not
-    #check xpower to see which index of polynomial to add to
-    return True
+def check(postforcing, xpower):
+    unforced = 0
+    for node in postforcing:
+        if node.infected == False:
+            unforced += 1
+    if unforced == 0:     #verify all nodes are forced
+        polynomial[xpower-1] += 1       #check xpower to see which index of polynomial to add to
 
 if __name__ == '__main__':
+    Node0 = Node(False, "0")
     Node1 = Node(False, "1")
-    Node2 = Node(True, "2")
+    Node2 = Node(False, "2")
     Node3 = Node(False, "3")
-    # Node4 = Node(False, "3")
-    # Node5 = Node(False, "3")
+    Node4 = Node(False, "4")
+    Node5 = Node(False, "5")
+    Node6 = Node(False, "6")
+    Node7 = Node(False, "7")
+    Node8 = Node(False, "8")
+    Node9 = Node(False, "9")
 
+    Node0.addNeighbor(Node1)
+    Node1.addNeighbor(Node0)
     Node1.addNeighbor(Node2)
     Node2.addNeighbor(Node1)
     Node2.addNeighbor(Node3)
     Node3.addNeighbor(Node2)
-    # Node3.addNeighbor(Node4)
-    # Node4.addNeighbor(Node5)
-
-    # test = [Node1, Node2, Node3]
-    # forcing(test)
+    Node3.addNeighbor(Node4)
+    Node4.addNeighbor(Node3)
+    Node6.addNeighbor(Node7)
+    Node7.addNeighbor(Node6)
+    Node7.addNeighbor(Node8)
+    Node8.addNeighbor(Node7)
+    Node8.addNeighbor(Node9)
+    Node9.addNeighbor(Node8)
+    Node4.addNeighbor(Node5)
+    Node5.addNeighbor(Node4)
+    Node5.addNeighbor(Node6)
+    Node6.addNeighbor(Node5)
 
     generatecombinations()
-    # infect(0)
-
+    print(polynomial)
 
